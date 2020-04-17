fc0ba682b6651a0c7184352c882359e4f145e6a6
InsertSomethingClever <40280272+InsertSomethingClever@users.noreply.github.com>
2020-04-17 05:50:40 -0600

    Update euler96.java

--- euler96.java
+++ euler96.java
@@ -6,6 +6,7 @@ class Scratch
 {
     private static int hold_data = 0;
     
+    
     public static void main(String[] args)
     {
         long start = System.nanoTime();
@@ -29,6 +30,7 @@ class Scratch
     
         while (fs.hasNextLine())
         {
+            // skip over the title lines...
             if (lineCount % 10 == 0)
             {
                 lineCount++;
@@ -36,25 +38,36 @@ class Scratch
             }
             else
             {
+                // each row in the sudoku
                 for (int i = 0; i < 9; i++)
                 {
                     lineCount++;
                     values = fs.nextLine().toCharArray();
+                    // each element in the row
+                    // make sure to convert the characters into integer values
                     for (int j = 0; j < 9; j++)
                         data[i][j] = (int)values[j] - (int)'0';
                 }
+                // call the solver function on the constructed sudoku data array
                 solve(data);
+                // clear the array to avoid possible data bugs
                 data = new int[9][9];
             }
         }
         
         long end = System.nanoTime();
-        System.out.printf("\nCompute Duration (s): %.4f", (end - start) / 1000000000.0);
+        System.out.printf("\nCompute Duration(s): %.4f", (end - start) / 1000000000.0);
         
         System.out.println("\n");
         System.out.println(hold_data);
     }
     
+    /**
+     * Recursively solve the sudoku. When a non-solution is found, return to the calling
+     * method and try a different value
+     *
+     * @param data reference to the sudoku data array
+     */
     public static void solve(int[][] data)
     {
         for (int i = 0; i < 9; i++)
@@ -74,18 +87,17 @@ class Scratch
         hold_data = hold_data + temp;
     }
     
+    /**
+     * Determine if the given value will fit in the space according to sudoku rules
+     *
+     * @param x column number
+     * @param y row number
+     * @param n value to check
+     * @param data reference to sudoku data array
+     * @return
+     */
     private static boolean viable(int x, int y, int n, int[][] data)
     {
-        // row
-        for (int e : data[x])
-            if (e == n)
-                return false;
-
-        // column
-        for (int[] e : data)
-            if (e[y] == n)
-                return false;
-        
         // box
         int x0 = x / 3;
         x0 = x0 * 3;
@@ -97,7 +109,17 @@ class Scratch
             for (int j = y0; j < y1; j++)
                 if (n == data[i][j])
                     return false;
-        
+       
+        // column
+        for (int[] e : data)
+            if (e[y] == n)
+                return false;
+                
+        // row
+        for (int e : data[x])
+            if (e == n)
+                return false;
+            
         return true;
     }
     
