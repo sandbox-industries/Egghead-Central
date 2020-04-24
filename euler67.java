ae819a7044b077be59092dbc00abc7d7a6c392b3
InsertSomethingClever <40280272+InsertSomethingClever@users.noreply.github.com>
2020-04-24 01:49:59 -0600

    add comments to file
    comments added to file to better describe what is happening.

--- euler67.java
+++ euler67.java
@@ -19,19 +19,29 @@ public class euler67
      */
     public static void main(String args[])
     {
+        // log the program run time start
         long start = System.currentTimeMillis();
         
+        // handle opening the file and building the binary tree
         processFile();
     
+        // build the path sum for each leaf of the binary tree. 
+        // The algorithm starts at the bottom and works towards the top
         for (elementNode e : prevNodes)
         {
+            // set the current path sum at the node to the value of the node
+            // this is only done for the leaves in the binary tree
             e.setNodeSum(e.getValue());
+            // calculate the path sums for the given leaf
             findPathSum(e);
         }
         
+        // log the program run time end
         long end = System.currentTimeMillis();
         
+        // calculate and log the duration of the program run
         System.out.println("Duration in ms: " + (end - start));
+        // print the results
         System.out.println(head);
     }
     
@@ -44,6 +54,7 @@ public class euler67
         Scanner fs = null;
         String lineValues[];
         
+        // open the file
         try
         {
             fs = new Scanner(data_file);
@@ -54,10 +65,14 @@ public class euler67
             System.exit(1);
         }
         
+        // read the file while there are lines left in the file
         while (fs.hasNextLine())
         {
+            // split the line into an array of strings with the space as the delimiter
             lineValues = fs.nextLine().split(" ");
+            // build the row in the data structure
             buildTree(lineValues, line);
+            // iterate the line counter, used for tracking the grid location of the element
             line++;
         }
     }
@@ -77,6 +92,7 @@ public class euler67
             elementNode curr = null;
             int v = Integer.parseInt(lineValues[i]);
             
+            // this is the first node
             if (prevNodes == null)
             {
                 curr = new elementNode(line, i, v, null, null);
@@ -85,16 +101,19 @@ public class euler67
             }
             else
             {
+                // there is no left parent
                 if (i == 0)
                 {
                     curr = new elementNode(line, i, v, null, prevNodes[i]);
                     prevNodes[i].setLeft(curr);
                 }
+                // there is no right parent
                 else if (i == lineValues.length - 1)
                 {
                     curr = new elementNode(line, i, v, prevNodes[i - 1], null);
                     prevNodes[i - 1].setRight(curr);
                 }
+                // there is both a left and right parent
                 else
                 {
                     curr = new elementNode(line, i, v, prevNodes[i - 1], prevNodes[i]);
@@ -109,33 +128,45 @@ public class euler67
     }
     
     /**
-     * Recursively builds the cumulative sum on a per element basis. This will not continue if the
-     * cumulative sum is less than the value already calculated for the node. This will skip over
-     * the paths which are not valid and less than another which has already "passed" through.
+     * Recursively builds the cumulative sum on a per element basis from the bottom to the top. This 
+     * will not continue if the cumulative sum is less than the value already calculated for the 
+     * node. This will skip over the paths which are not valid and less than another which has 
+     * already "passed" through.
      *
      * @param step elementNode, the current node the function is processing
      */
     private static void findPathSum(elementNode step)
     {
+        // initialize left and right path sum values
         int left = 0;
         int right = 0;
 
+        // do not continue left if there is nothing to the left
         if (step.getParentLeft() != null)
         {
+            // calculate the path sum for the left parent
             left = step.getParentLeft().getValue() + step.getNodeSum();
+            // if the path sum is less than the already set value, do not continue recursion
             if (left > step.getParentLeft().getNodeSum())
             {
+                // set the path sum for the node
                 step.getParentLeft().setNodeSum(left);
+                // continue calculating the path sum starting from the left parent
                 findPathSum(step.getParentLeft());
             }
         }
 
+        // do not continue to the right if there is nothing to the right
         if (step.getParentRight() != null)
         {
+            // calculate the path sum for the right parent
             right = step.getParentRight().getValue() + step.getNodeSum();
+            // if the path sum is less than the already set value, do not continue recursion
             if (right > step.getParentRight().getNodeSum())
             {
+                // set the path sum for the node
                 step.getParentRight().setNodeSum(right);
+                // continue calculating the path sum starting from the right parent
                 findPathSum(step.getParentRight());
             }
         }
