bd038c8b91b0d29d82326c0e00681a35d33aa8ff
InsertSomethingClever <40280272+InsertSomethingClever@users.noreply.github.com>
2020-06-16 22:44:35 -0600

    Update pe54.py

--- pe54/cleaned/pe54.py
+++ pe54/cleaned/pe54.py
@@ -6,37 +6,26 @@ from card import Card
 card_map = {'2': 2,  '3': 3,  '4': 4,  '5': 5,  '6': 6,  '7': 7, '8': 8, '9': 9,
             'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14, }
 
-# container for the number of times that player 1 wins
 p1_wins = 0
 
-# read the text file containing pairs of hands
 with open('pe54.txt', 'r') as file:
     dat = file.readlines()
 
 start = time.time()
 
-# run through each pair of hands (each line contains one pair of hands)
 for line in dat:
-    # define the containers for the hands of player 1 and player 2
     p1 = Hand()
     p2 = Hand()
 
-    # split the cards by the spaces between them
     cards = line.split(" ")
 
     # run through the cards for player 1
-    for i in range(5):
-        val = card_map[cards[i][0]]
-        suit = cards[i][1]
-        card = Card(val, suit)
-        p1.add_card(card)
+    for x in cards[:5]:
+        p1.add_card(Card(card_map[x[0]], x[1]))
 
     # run through the cards for player 2
-    for i in range(5, 10):
-        val = card_map[cards[i][0]]
-        suit = cards[i][1]
-        card = Card(val, suit)
-        p2.add_card(card)
+    for x in cards[5:]:
+        p2.add_card(Card(card_map[x[0]], x[1]))
 
     # Determine who won the hand based on the calculated values.
     # highest hand value wins, a tie proceeds to compare the pair values (if there is a pair)
@@ -55,6 +44,5 @@ for line in dat:
 
 stop = time.time()
 print("duration (sec):", stop-start)
-# Project Euler only cares about how many games player 1 wins in the given file
 print(p1_wins)
 
