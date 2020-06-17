b7e2a1e8f7da714e1f55373a3eba8db03052ace5
InsertSomethingClever <40280272+InsertSomethingClever@users.noreply.github.com>
2020-06-17 09:29:06 -0600

    Update card.py

--- pe54/cleaned/card.py
+++ pe54/cleaned/card.py
@@ -1,10 +1,23 @@
 class Card:
     def __init__(self, value=None, suit=None):
+        """
+        Initialize the card object
+        :param value: int or none, integer value of the card
+        :param suit:  str or none, character value of the suit of the card
+        """
         self._value = value
         self._suit = suit
 
-    def get_value(self):
+    def get_value(self) -> int:
+        """
+        Public function to return the value of the card object
+        :return: int, the value of the card object
+        """
         return self._value
 
-    def get_suit(self):
+    def get_suit(self) -> str:
+        """
+        Public function to return the suit of the card
+        :return: str, the character representing the suit of the card
+        """
         return self._suit
